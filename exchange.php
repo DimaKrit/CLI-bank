b3a7d95527efebab5a8c1a7898a7dcdad98e59ba
dimakrit <chemodan2@ukr.net>
2019-02-03 11:37:22 +0200

    add error code

--- exchange.php
+++ exchange.php
@@ -1,10 +1,19 @@
 <?php
 
-if (!isset($argv[1])) exit('Enter the required amount');
+if (!isset($argv[1])) {
+	echo 'Enter the required amount';
+	exit(1);
+}
 
-if (!is_numeric($argv[1])) exit('Enter an integer');
+if (!is_numeric($argv[1])) {
+	echo 'Enter an integer';
+	exit(1);
+}
 
-if ($argv[1] > 100000) exit('Maximum amount 100000');
+if ($argv[1] > 100000) {
+	echo 'Maximum amount 100000';
+	exit(1);
+}
 
 $nominalArray = [1, 2, 5, 10, 20, 50, 100, 200, 500];
 
